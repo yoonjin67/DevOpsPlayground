eeaefcb74ec2be037a0216a5c60ed70de3c1fd2b
Lee Yunjin <gg582@naver.com>
2025-06-22 17:14:32 +0900

    letsencrypt installation scripts SHOULD NOT include other project's certification process

--- Kubernetes/letsencrypt/scripts/install.sh
+++ Kubernetes/letsencrypt/scripts/install.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.5/cert-manager.yaml
 kubectl apply -f $(YAML $GITLAB_PLAYGROUND)/letsencrypt
-kubectl annotate ingress gitlab-webservice-default -n gitlab cert-manager.io/cluster-issuer="letsencrypt-prod" nginx.ingress.kubernetes.io/ssl-redirect="false" --overwrite
-kubectl annotate ingress gitlab-minio -n gitlab cert-manager.io/cluster-issuer="letsencrypt-prod" nginx.ingress.kubernetes.io/ssl-redirect="false" --overwrite
-kubectl annotate ingress gitlab-kas -n gitlab cert-manager.io/cluster-issuer="letsencrypt-prod" nginx.ingress.kubernetes.io/ssl-redirect="false" --overwrite
-kubectl annotate ingress gitlab-registry -n gitlab cert-manager.io/cluster-issuer="letsencrypt-prod" nginx.ingress.kubernetes.io/ssl-redirect="false" --overwrite
